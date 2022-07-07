99d427fce1a5cd2d1dde70e82a9ff88da17fc2c7
andrew farquharson <farquharson.a@gmail.com>
2022-07-07 22:14:10 +0100

    php syntax

--- web/modules/custom/amdj_message/src/AmdjMessageGreeting.php
+++ web/modules/custom/amdj_message/src/AmdjMessageGreeting.php
@@ -3,15 +3,13 @@
 namespace Drupal\amdj_message;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
 use Drupal\Core\PageCache\ResponsePolicyInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Prepares the greeting.
  */
-class AmdjMessageGreeting
-{
+class AmdjMessageGreeting {
   use StringTranslationTrait;
 
   /**
@@ -29,36 +27,32 @@ class AmdjMessageGreeting
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    * @param \Drupal\Core\PageCache\ResponsePolicyInterface $kill_switch
    */
-  public function __construct(ConfigFactoryInterface $config_factory, KillSwitch $kill_switch)
-  {
+  public function __construct(ConfigFactoryInterface $config_factory, ResponsePolicyInterface $kill_switch) {
     $this->configFactory = $config_factory;
     $this->killSwitch = $kill_switch;
   }
 
   /**
-   * Returns the greeting
+   * Returns the greeting.
    */
-  public function getGreeting()
-  {
+  public function getGreeting() {
     $this->killSwitch->trigger();
     $config = $this->configFactory->get('amdj_message.custom_greeting');
     $greeting = $config->get('greeting');
-    if ($greeting !== "" && $greeting)
-    {
+    if ($greeting !== "" && $greeting) {
       return $greeting;
     }
     $time = new \DateTime();
-    if ((int)$time->format('G') >= 00
-      && (int)$time->format('G') < 12) {
+    if ((int) $time->format('G') >= 00
+      && (int) $time->format('G') < 12) {
       return $this->t('Good morning world');
     }
-    if ((int)$time->format('G') >= 12
-      && (int)$time->format('G') < 18) {
+    if ((int) $time->format('G') >= 12
+      && (int) $time->format('G') < 18) {
       return $this->t("Good afternoon world");
     }
-    if ((int)$time->format('G') >= 18) {
+    if ((int) $time->format('G') >= 18) {
       return $this->t('Good evening world');
     }
   }
-
 }
