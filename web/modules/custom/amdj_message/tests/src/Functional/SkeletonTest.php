53ebaa3c52e923dc0960872d88c4339c44a1ef3c
andrew farquharson <farquharson.a@gmail.com>
2022-07-08 19:38:13 +0100

    fix skeleton test

--- web/modules/custom/amdj_message/tests/src/Functional/SkeletonTest.php
+++ web/modules/custom/amdj_message/tests/src/Functional/SkeletonTest.php
@@ -14,12 +14,13 @@ class SkeletonTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-	 protected $defaultTheme = 'stark';
+  protected $defaultTheme = 'stark';
 
   /**
    * This test method fails, so we can be sure our test is discovered.
    */
-	 public function testFail() {
-		 $this->fail('The test runner found our test and failed it. Yay!');
-   }
+  public function testFail() {
+    $this->fail('The test runner found our test and failed it. Yay!');
+  }
+
 }
