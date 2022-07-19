8f53c5bb13b4b62f8a094e6e38f935bed1d3dc31
andrew farquharson <farquharson.a@gmail.com>
2022-07-19 12:14:48 +0100

    create a service

---
+++ web/modules/custom/dino_roar/src/Controller/RoarController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\dino_roar\Controller;
+
+use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoarController {
+  public function roar($count) {
+    $roarGenerator = new RoarGenerator();
+    $roar = $roarGenerator->getRoar($count);
+    return new Response($roar);
+  }
+}
