544c389a1a5bc44bb0bb2e87e74ced3282b9b108
andrew farquharson <farquharson.a@gmail.com>
2022-08-17 17:12:54 +0100

    logger logs to dblog

--- web/modules/custom/amdj_message/src/Form/GreetingConfigurationForm.php
+++ web/modules/custom/amdj_message/src/Form/GreetingConfigurationForm.php
@@ -4,12 +4,45 @@ namespace Drupal\amdj_message\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Configuration form definition for the greeting message
+ * Configuration form definition for the greeting message.
  */
 class GreetingConfigurationForm extends ConfigFormBase
 {
+  /**
+   * The logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * SalutationConfigurationForm constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
+   *   The logger.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, LoggerChannelInterface $logger) {
+    parent::__construct($config_factory);
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('amdj_message.logger.channel.amdj_message')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -43,9 +76,12 @@ class GreetingConfigurationForm extends ConfigFormBase
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-    $this->config('amdj_message.custom_greeting')->set('greeting', $form_state->getValue('greeting'))
+    $this->config('amdj_message.custom_greeting')
+      ->set('greeting', $form_state->getValue('greeting'))
       ->save();
     parent::submitForm($form, $form_state);
+    $this->logger->info('The Amdj Message salutation has been changed to @message.', ['@message' => $form_state->getValue('greeting')]);
+
   }
 
   /**
